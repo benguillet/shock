fd60669ba7c31c509f756b6c0ada46a8c2b9089a
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-04-11 03:36:58 -0500

    task scheduled

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
+set :environment, 'development'
+set :output, '~/Dropbox/Projects/perso/shock/logs/import.log'
+set :job_template, "zsh ':job'"
+
 every 1.minute do
-  rake "earthquake:import"
+  rake 'earthquake:import'
 end
--- shock.rb
+++ shock.rb
@@ -2,9 +2,18 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' 
 require './models/earthquake'
+require 'poncho'
 
 class Shock  < Sinatra::Base
   get '/' do 
     "Hello, World!"
   end
+
+  class GetEarthquakeMethod < Poncho::JSONMethod
+    def invoke
+      @earthquakes = Earthquake.all
+    end
+  end
+
+ get '/earthquakes.json', &GetEarthquakeMethod
 end
