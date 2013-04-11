8e2baa30818aa37c8043f9a1123fd4764cd258d1
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-04-11 03:41:07 -0500

    fix migration for heroku

--- db/migrate/20130411055544_complete_earthquakes_table.rb
+++ db/migrate/20130411055544_complete_earthquakes_table.rb
@@ -6,7 +6,7 @@ class CompleteEarthquakesTable < ActiveRecord::Migration
       t.index :eqid, :unique => true
       t.string :source, :null => false, :default => ""
       t.integer :version, :null => false, :default => 0
-      t.datetime :datetime, :null => false, :default => ""
+      t.datetime :datetime, :null => false, :default => Time.now
       t.decimal :latitude, :precision => 7, :scale => 4, :null => false, :default => 0
       t.decimal :longitude, :precision => 7, :scale => 4, :null => false, :default => 0
       t.decimal :magnitude, :precision => 2, :scale => 1, :null => false, :default => 0
