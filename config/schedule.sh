ce960558a8e8f68c0cba52db934af97d0b56eab4
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-04-15 03:46:16 -0500

    very small improvement of shedule.sh

--- config/schedule.sh
+++ config/schedule.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 export PATH=$HOME/.rbenv/shims:/usr/local/share/python:/usr/local/heroku/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
-cd /Users/tartopom/Dropbox/Projects/perso/shock && RAILS_ENV=development bundle exec rake earthquake:import --silent >> ~/Dropbox/Projects/perso/shock/log/import.log 2>&1
+cd ~/Dropbox/Projects/perso/shock && RAILS_ENV=development bundle exec rake earthquake:import --silent >> ~/Dropbox/Projects/perso/shock/log/import.log 2>&1
